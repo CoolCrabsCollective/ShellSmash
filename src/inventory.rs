9d62efbeb0bf1533be7aa8bb1e2be9fda693bec0
Alexander Winter <a.w1nter@hotmail.com>
2023-09-29 21:34:44 -0400

    Fixed InventoryItem

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,5 +1,25 @@
 use bevy::prelude::*;
 
 pub struct InventoryItem {
-    pub points: Vec<Vec2>,
+    pub location: IVec3,    // world location
+    pub points: Vec<IVec3>, // relative coordinate
+}
+
+impl InventoryItem {
+    fn spawn_cubes(
+        &self,
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<StandardMaterial>>,
+    ) {
+        for point in &self.points {
+            // cube
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                transform: Transform::from_xyz(point.x as f32, point.y as f32, point.y as f32),
+                ..default()
+            });
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod inventory;
 
+use crate::inventory::InventoryItem;
 use bevy::prelude::*;
 
 fn main() {
@@ -43,4 +44,8 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+
+    //let item = InventoryItem {
+    //    points: vec![(0, 10, 0), (0, 10, 0)],
+    //};
 }
