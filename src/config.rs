db9466f5eb6cf6fe6e64b2b540768abc7bfaaebd
David Huculak <davidhuculak5@gmail.com>
2023-10-01 10:45:05 -0400

    add physics world debug constant

--- src/config.rs
+++ src/config.rs
@@ -3,3 +3,5 @@ pub const VOXEL_RENDERER_LEFT_RIGHT_CONTROLS: bool = false;
 pub const INVENTORY_GRID_DIMENSIONS: [i32; 3] = [7, 2, 7];
 
 pub const SPAWN_ENEMIES: bool = false;
+
+pub const ENABLE_DEBUG_PHYSICS_WORLD: bool = false;
