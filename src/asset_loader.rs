e842baee0bf2b0d65ff5517175c519603a04483a
David Huculak <davidhuculak5@gmail.com>
2023-10-01 14:59:55 -0400

    enemy asset load in background

--- src/asset_loader.rs
+++ src/asset_loader.rs
@@ -1,5 +1,6 @@
 use bevy::{
-    asset::{Asset, LoadState},
+    asset::{LoadState, SourcePathId},
+    gltf::{Gltf, GltfMesh},
     log,
     prelude::*,
 };
@@ -10,87 +11,62 @@ const JELLY_ASSET_PATH: &str = "jelly.glb#Scene0";
 
 pub struct AssetLoaderPlugin;
 
-// TODO: create pending version
-#[derive(Default, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct LoadedSingleModelScene {
-    pub scene_handle: Option<Handle<Scene>>,
-    pub mesh_handle: Option<Handle<Mesh>>,
-    pub material_handle: Option<Handle<StandardMaterial>>,
+    pub scene_handle: Handle<Gltf>,
+    pub mesh_handle: Handle<Mesh>,
+    pub material_handle: Handle<StandardMaterial>,
+    // pub transform: Transform,
 }
 
 #[derive(Resource, Default)]
 pub struct GameAssets {
-    jelly: LoadedSingleModelScene,
+    jelly_scene_handle: Handle<Gltf>,
+    jelly: Option<LoadedSingleModelScene>,
 }
 
 impl GameAssets {
     pub fn are_all_assets_loaded(&self) -> bool {
-        self.jelly.is_loaded()
+        self.jelly.is_some()
     }
 
     pub fn jelly(&self) -> LoadedSingleModelScene {
-        assert!(self.jelly.is_loaded());
-        self.jelly.clone()
-    }
-}
-
-impl LoadedSingleModelScene {
-    pub fn is_loaded(&self) -> bool {
-        self.scene_handle.is_some() && self.mesh_handle.is_some() && self.material_handle.is_some()
+        self.jelly.clone().unwrap()
     }
 }
 
 impl Plugin for AssetLoaderPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, (load_all_game_assets, handle_scene_load_event));
-        app.add_systems(
-            Update,
-            handle_scene_load_event.run_if(in_state(GameState::Loading)),
-        );
-        app.add_systems(
-            Update,
-            handle_mesh_load_event.run_if(in_state(GameState::Loading)),
-        );
+        app.add_systems(Update, handle_scene_load_event);
         app.insert_resource(GameAssets::default());
     }
 }
 
-fn load_all_game_assets(mut asset_server: ResMut<AssetServer>) {
-    let _ignored_scene_handle: Handle<Scene> = asset_server.load(JELLY_ASSET_PATH);
+fn load_all_game_assets(asset_server: Res<AssetServer>, mut game_assets: ResMut<GameAssets>) {
+    game_assets.jelly_scene_handle = asset_server.load(JELLY_ASSET_PATH);
 }
 
 fn handle_scene_load_event(
-    mut load_events: EventReader<AssetEvent<Scene>>,
+    mut load_events: EventReader<AssetEvent<Gltf>>,
     mut game_assets: ResMut<GameAssets>,
     mut game_state_updater: ResMut<NextState<GameState>>,
     asset_server: Res<AssetServer>,
-    scenes: Res<Assets<Scene>>,
-    meshes: Res<Assets<Mesh>>,
-    materials: Res<Assets<StandardMaterial>>,
-    mesh_handle_query: Query<&Handle<Mesh>>,
-    material_handle_query: Query<&Handle<StandardMaterial>>,
+    scenes: Res<Assets<Gltf>>,
+    gltf_meshes: Res<Assets<GltfMesh>>,
 ) {
     for event in load_events.iter() {
         if let AssetEvent::Created {
             handle: scene_handle,
         } = event
         {
-            // Failed
             match asset_server.get_load_state(scene_handle) {
                 LoadState::Loaded => {
-                    if asset_server
-                        .get_handle_path(scene_handle)
-                        .expect("Couldn't get asset path?")
-                        == JELLY_ASSET_PATH.into()
-                    {
-                        dbg!(asset_server.get_handle_path(scene_handle));
+                    if is_same_asset(scene_handle.clone(), game_assets.jelly_scene_handle.clone()) {
                         let (mesh_handle, material_handle) = get_first_mesh_material_from_scene(
                             scene_handle.clone(),
                             &scenes,
-                            &meshes,
-                            &materials,
-                            &mesh_handle_query,
-                            &material_handle_query,
+                            &gltf_meshes,
                         );
                         game_assets.jelly = Some(LoadedSingleModelScene {
                             scene_handle: scene_handle.clone(),
@@ -100,12 +76,13 @@ fn handle_scene_load_event(
                     }
                 }
                 LoadState::Failed => {
-                    log::error!("scene failed to load dog");
+                    log::error!(
+                        "scene failed to load dog: asset path = {:?}",
+                        asset_server.get_handle_path(scene_handle)
+                    );
                 }
                 _ => {}
             }
-
-            // if handle.
         }
     }
 
@@ -114,103 +91,54 @@ fn handle_scene_load_event(
     }
 }
 
-fn handle_mesh_load_event(
-    mut load_events: EventReader<AssetEvent<Mesh>>,
-    mut game_assets: ResMut<GameAssets>,
-    mut game_state_updater: ResMut<NextState<GameState>>,
-    asset_server: Res<AssetServer>,
-    scenes: Res<Assets<Scene>>,
-    meshes: Res<Assets<Mesh>>,
-    materials: Res<Assets<StandardMaterial>>,
-    mesh_handle_query: Query<&Handle<Mesh>>,
-    material_handle_query: Query<&Handle<StandardMaterial>>,
-) {
-    for event in load_events.iter() {
-        if let AssetEvent::Created {
-            handle: scene_handle,
-        } = event
-        {
-            // Failed
-            match asset_server.get_load_state(scene_handle) {
-                LoadState::Loaded => {
-                    if asset_server
-                        .get_handle_path(scene_handle)
-                        .expect("Couldn't get asset path?")
-                        == JELLY_ASSET_PATH.into()
-                    {
-                        dbg!(asset_server.get_handle_path(scene_handle));
-                        let (mesh_handle, material_handle) = get_first_mesh_material_from_scene(
-                            scene_handle.clone(),
-                            &scenes,
-                            &meshes,
-                            &materials,
-                            &mesh_handle_query,
-                            &material_handle_query,
-                        );
-                        game_assets.jelly = Some(LoadedSingleModelScene {
-                            scene_handle: scene_handle.clone(),
-                            mesh_handle,
-                            material_handle,
-                        });
-                    }
-                }
-                LoadState::Failed => {
-                    log::error!("scene failed to load dog");
-                }
-                _ => {}
-            }
-
-            // if handle.
-        }
+fn is_same_asset(asset_handle_a: Handle<Gltf>, asset_handle_b: Handle<Gltf>) -> bool {
+    match (
+        get_source_path_id(asset_handle_a),
+        get_source_path_id(asset_handle_b),
+    ) {
+        (Some(a), Some(b)) => a == b,
+        _ => false,
     }
+}
 
-    if game_assets.are_all_assets_loaded() {
-        game_state_updater.set(GameState::FightingInArena);
+fn get_source_path_id(asset: Handle<Gltf>) -> Option<SourcePathId> {
+    match asset.id() {
+        bevy::asset::HandleId::Id(_, _) => None,
+        bevy::asset::HandleId::AssetPathId(asset_path_id) => Some(asset_path_id.source_path_id()),
     }
 }
 
 fn get_first_mesh_material_from_scene(
-    scene_handle: Handle<Scene>,
-    scenes: &Res<Assets<Scene>>,
-    meshes: &Res<Assets<Mesh>>,
-    materials: &Res<Assets<StandardMaterial>>,
-    mesh_handle_query: &Query<&Handle<Mesh>>,
-    material_handle_query: &Query<&Handle<StandardMaterial>>,
+    scene_handle: Handle<Gltf>,
+    scenes: &Res<Assets<Gltf>>,
+    gltf_meshes: &Res<Assets<GltfMesh>>,
 ) -> (Handle<Mesh>, Handle<StandardMaterial>) {
     let scene = scenes.get(&scene_handle).unwrap();
 
-    let first_entity = scene
-        .world
-        .iter_entities()
-        .next()
-        .expect("No objects found in scene");
+    let mesh_handle = gltf_meshes
+        .get(
+            scene
+                .meshes
+                .get(0)
+                .expect("Scene must have at least one mesh"),
+        )
+        .unwrap()
+        .primitives[0]
+        .mesh
+        .clone();
 
-    let mesh_handle = mesh_handle_query
-        .get(first_entity.id())
-        .expect("First object had no mesh")
-        .clone();
-    let material_handle = material_handle_query
-        .get(first_entity.id())
-        .expect("First object had no material")
-        .clone();
+    let material_handle = gltf_meshes
+        .get(
+            scene
+                .meshes
+                .get(0)
+                .expect("Scene must have at least one mesh"),
+        )
+        .unwrap()
+        .primitives[0]
+        .material
+        .clone()
+        .expect("Mesh must have a material");
 
     (mesh_handle, material_handle)
-}
-
-fn asset_is_loaded(
-    asset: Option<LoadedSingleModelScene>,
-    asset_server: &mut ResMut<AssetServer>,
-) -> bool {
-    asset.is_some() && asset_server.get_load_state(asset.unwrap().scene_handle) == LoadState::Loaded
-}
-
-fn bevy_asset_is_loaded<T>(
-    asset_handle: Option<Handle<T>>,
-    asset_server: &mut ResMut<AssetServer>,
-) -> bool
-where
-    T: Asset,
-{
-    asset_handle.is_some()
-        && asset_server.get_load_state(asset_handle.unwrap()) == LoadState::Loaded
 }
