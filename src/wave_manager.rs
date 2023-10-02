05f9a571abcd6c546d2386d74fac013d4e0d230f
William Wells <williamwells2013@hotmail.com>
2023-10-02 09:29:02 -0400

    Implement wave states

--- src/wave_manager.rs
+++ src/wave_manager.rs
@@ -16,6 +16,14 @@ pub const ARENA_DIMENSIONS_METERS: [f32; 2] = [24.0, 30.0];
 
 pub struct WaveManagerPlugin;
 
+#[derive(Debug, Clone, Default, Eq, PartialEq, Hash, States)]
+pub enum WaveState {
+    #[default]
+    NO_WAVE,
+    ACTIVE_WAVE,
+    WAVE_END,
+}
+
 #[derive(Resource)]
 struct SpawnTimer(Timer);
 
@@ -24,10 +32,20 @@ struct WaveStartDelayTimer(Timer);
 
 impl Plugin for WaveManagerPlugin {
     fn build(&self, app: &mut App) {
+        app.add_state::<WaveState>();
         app.add_systems(OnEnter(GameState::FightingInArena), reset_start_delay);
+
         app.add_systems(
             Update,
-            spawn_enemies.run_if(in_state(GameState::FightingInArena)),
+            wait_for_wave_start
+                .run_if(in_state(GameState::FightingInArena))
+                .run_if(in_state(WaveState::NO_WAVE)),
+        );
+        app.add_systems(
+            Update,
+            spawn_enemies
+                .run_if(in_state(GameState::FightingInArena))
+                .run_if(in_state(WaveState::ACTIVE_WAVE)),
         );
 
         app.insert_resource(WaveStartDelayTimer(Timer::from_seconds(
@@ -39,7 +57,17 @@ impl Plugin for WaveManagerPlugin {
 }
 
 fn reset_start_delay(mut start_delay_timer: ResMut<WaveStartDelayTimer>) {
-    start_delay_timer.0.reset();
+    // start_delay_timer.0.reset();
+}
+
+fn wait_for_wave_start(
+    mut start_delay_timer: ResMut<WaveStartDelayTimer>,
+    time: Res<Time>,
+    mut next_state: ResMut<NextState<WaveState>>,
+) {
+    if start_delay_timer.0.tick(time.delta()).finished() {
+        next_state.set(WaveState::ACTIVE_WAVE)
+    }
 }
 
 fn spawn_enemies(
@@ -52,10 +80,6 @@ fn spawn_enemies(
     time: Res<Time>,
 ) {
     if !SPAWN_ENEMIES {
-        return;
-    }
-
-    if !start_delay_timer.0.tick(time.delta()).finished() {
         return;
     }
 
