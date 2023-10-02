5d310f5f408b04f7ac5c0fd42f0236fd8561b828
David Huculak <davidhuculak5@gmail.com>
2023-10-02 17:30:42 -0400

    switch release build for web

--- build_web.sh
+++ build_web.sh
@@ -1,7 +1,7 @@
 # crate_name=cargo tree --depth 0 | awk '{print $1;}'
 rm -rf ./out
-cargo build --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./out/ --target web target/wasm32-unknown-unknown/debug/ld54.wasm
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --out-dir ./out/ --target web target/wasm32-unknown-unknown/release/shell_smash.wasm
 cp index.html ./out
 mkdir ./out/assets/
 cp -r ./assets/* ./out/assets/
